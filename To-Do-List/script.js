cd9dd9ecc4f2ef9b3b059b9f33470bb9ee2330b2
Karthikeyan R <143987542+karthikeyan10122k3@users.noreply.github.com>
2023-11-06 21:57:30 +0530

    Add files via upload

---
+++ To-Do-List/script.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("input");
+const list = document.getElementById("task-list");
+
+function addingTask(){
+  if(input.value === ''){
+    alert("Nothing Writen");
+  }
+  else{
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    list.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  input.value = "";
+} 
+
+list.addEventListener("click",function(e){
+  if(e.target.tagName ==="LI"){
+    e.target.classList.toggle("checked");
+  }
+  else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+  }
+
+}, false);
+
